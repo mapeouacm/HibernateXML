74bc8fe0707eadbf9c05c740cb4d4f12b97f3822
mapeouacm <mapeo.uacm@gmail.com>
2021-03-16 01:54:56 -0600

    Se agrego proyecto con Mapeo XML de Hibernate
    Se agregó ejemplo de mapeo con puro hibernate utilizando archivos XML.

---
+++ src/main/java/mx/edu/uacm/hibernatexml/dominio/Usuario.java
@@ -0,0 +1,39 @@
+package mx.edu.uacm.hibernatexml.dominio;
+
+public class Usuario {
+	
+	private int id;
+	
+	private String nombreUsuario;
+	
+	private String mensajeUsuario;
+	
+	public Usuario() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getMensajeUsuario() {
+		return mensajeUsuario;
+	}
+
+	public void setMensajeUsuario(String mensajeUsuario) {
+		this.mensajeUsuario = mensajeUsuario;
+	}
+	
+}
